d3fc9254655297642b5fa252523add6b81ba9c08
G SEKHAR REDDY <sekhar.reddy.cse.2023@nist.edu>
2025-06-02 11:59:46 +0530

    Creating the add details UI

--- app/src/main/java/com/example/sqllitedatabase/AddRecord.java
+++ app/src/main/java/com/example/sqllitedatabase/AddRecord.java
@@ -1,9 +1,10 @@
 package com.example.sqllitedatabase;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +12,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-	Button b1, b2;
+public class AddRecord extends AppCompatActivity {
+
+	Button b1;
+	Spinner sp;
+	String name, qual, dob;
+	ArrayList list = new ArrayList<>;
+	EditText et1, et2;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		EdgeToEdge.enable(this);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_add_record);
 
 		b1 = findViewById(R.id.b1);
-		b2 = findViewById(R.id.b2);
+		sp = findViewById(R.id.sp);
+		et1 = findViewById(R.id.et1);
+		et2 = findViewById(R.id.et2);
+		list.add("Matriculation");
 
 		b1.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, AddRecord.class);
-				startActivity(intent);
+
 			}
 		});
-		b2.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, ViewRecord.class);
-				startActivity(intent);
-			}
-		});
+
+
 		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 			Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
